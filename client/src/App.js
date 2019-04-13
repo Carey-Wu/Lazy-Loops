f4d39745b96b1e63db581a3f2cc7c622e5563dc3
Carey Wu <cwu@emilcott.com>
2019-04-13 13:55:44 -0400

    converted Rogers html to React

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import FinalGrid from "./components/BeatGrid/FinalGrid"
-import Keyboard from "./components/Keyboard"
+import Dashboard from './Pages/Dashboard';
+import LandingPage from './Pages/LandingPage';
 
-function App () {
-  return(
-    <div>
-    <FinalGrid />
-    <Keyboard />
-    </div>
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route
+            exact path="/dashboard"
+            component={Dashboard}
+            />
+            <Route
+            exact path="/"
+            component={LandingPage}
+            />
+        </Switch>
+      </div>
+    </Router>
+
   )
 }
 

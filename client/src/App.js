586a0a14a7c3a279b6a69d0fe59c3f5df48d7bbe
Carey Wu <cwu@emilcott.com>
2019-04-13 11:47:59 -0400

    adding Kyle's keyboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import FinalGrid from "./components/BeatGrid/FinalGrid"
+import Keyboard from "./components/Keyboard"
 
 function App () {
   return(
+    <div>
     <FinalGrid />
+    <Keyboard />
+    </div>
   )
 }
 

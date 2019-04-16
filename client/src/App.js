c862d0e5049490395654314ad0a4dbfd54edb2ba
Toad Sage <kylenewins@gmail.com>
2019-04-16 18:33:08 -0400

    different keyboard sounds

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './App.css';
 import FinalGrid from "./components/BeatGrid/FinalGrid"
 import Keyboard from "./components/Keyboard"
+const Tone = require("tone")
+const defaultSynth = new Tone.Synth().toMaster()
+const AMSynth = new Tone.AMSynth().toMaster()
+const DuoSynth = new Tone.DuoSynth().toMaster()
+const FMSynth = new Tone.FMSynth().toMaster()
+const MonoSynth = new Tone.MonoSynth().toMaster()
+const PluckSynth = new Tone.PluckSynth().toMaster()
+const MembraneSynth = new Tone.MembraneSynth().toMaster()
+const synths = [defaultSynth, AMSynth, DuoSynth, FMSynth, MonoSynth, PluckSynth, MembraneSynth]
+
 
 function App () {
+
   return(
     <div>
     <FinalGrid />
-    <Keyboard />
+    <Keyboard synths={synths}/>
     </div>
   )
 }

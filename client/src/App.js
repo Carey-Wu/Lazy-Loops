3303525e7ae31a84e357e9bbb1cb4df962b7584d
Toad Sage <kylenewins@gmail.com>
2019-04-16 20:06:16 -0400

    merge fix

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,7 @@ import FinalGrid from "./components/BeatGrid/FinalGrid"
 import Keyboard from "./components/Keyboard"
 import Dashboard from './Pages/Dashboard';
 import LandingPage from './Pages/LandingPage';
-const Tone = require("tone")
-const defaultSynth = new Tone.Synth().toMaster()
-const AMSynth = new Tone.AMSynth().toMaster()
-const DuoSynth = new Tone.DuoSynth().toMaster()
-const FMSynth = new Tone.FMSynth().toMaster()
-const MonoSynth = new Tone.MonoSynth().toMaster()
-const PluckSynth = new Tone.PluckSynth().toMaster()
-const MembraneSynth = new Tone.MembraneSynth().toMaster()
-const synths = [defaultSynth, AMSynth, DuoSynth, FMSynth, MonoSynth, PluckSynth, MembraneSynth]
+
 
 
 
